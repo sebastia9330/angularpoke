9cc345c96a2520d807e97b9cdf9b3aaf82e3d46a
sebastian <mugssebastian@gmail.com>
2023-04-23 20:14:59 -0500

    primer componente

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './paginas/inicio/inicio.component';
+import { FotoPokemonComponent } from './components/foto-pokemon/foto-pokemon.component';
+import { TarjetaPokemonComponent } from './components/tarjeta-pokemon/tarjeta-pokemon.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    FotoPokemonComponent,
+    TarjetaPokemonComponent,
+    DetalleComponent,
   ],
   imports: [
     BrowserModule,
